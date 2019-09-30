63bbcd9f5fff34df9f174d4ca3a8cb7e6b114b7a
Diego <diegofss11@gmail.com>
2019-09-30 13:54:55 +1000

    Fix clashing space

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 
 export default () => (
   <header className='header'>
-    <h1>Logo</h1>
-    <nav>
-      <ul>
-        <li>Home</li>
-      </ul>
-    </nav>
+    <h1>Microfront ends</h1>
   </header>
 );
