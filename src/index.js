67db7a8cd28d51a3eb73a513087b6d2793e880ed
Diego <diegofss11@gmail.com>
2019-10-17 17:32:57 +1100

    Revert cbd4af0f3385b011a7e0448ecf6b92e83910d7ca

--- src/index.js
+++ src/index.js
@@ -3,10 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-class Header extends HTMLElement {
-    attachedCallback() {
-        ReactDOM.render(<App />, this.createShadowRoot());
-    }
-}
-
-document.registerElement('microfrontend-header', Header);
+ReactDOM.hydrate(<App />, document.getElementById('header-root'));
